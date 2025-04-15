b9d90c29c02ff6e5b8bb66cb6fca30f5ebb9fab7
Sachin Singh <122505800+Sachin-fsd@users.noreply.github.com>
2025-04-15 09:41:27 +0530

    updated: removed console.log

--- middleware/authenticator.middleware.js
+++ middleware/authenticator.middleware.js
@@ -13,7 +13,7 @@ const authenticator = async (req, res, next) => {
     }
     jwt.verify(token, process.env.secret_key, async (err, decoded) => {
       if (err) {
-        console.log("auth token started", req.url, req.route, "line 10");
+        // console.log("auth token started", req.url, req.route, "line 10");
         return res.redirect("/welcome");
       } else {
         req.user = decoded.UserDetails
